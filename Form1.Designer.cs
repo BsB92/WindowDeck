e1bfd6f0af635d1c87ae99fbd61a6e3ec5b78107
BsB92 <106185452+BsB92@users.noreply.github.com>
2026-09-24 22:13:38 +0200

    Improve flyout toolbar and presentation button layout

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,11 +43,11 @@ namespace WindowDeck
             // searchTextBox
             //
             searchTextBox.Dock = DockStyle.Fill;
-            searchTextBox.Location = new Point(0, 4);
-            searchTextBox.Margin = new Padding(0, 4, 8, 4);
+            searchTextBox.Location = new Point(0, 44);
+            searchTextBox.Margin = new Padding(0, 4, 0, 4);
             searchTextBox.Name = "searchTextBox";
             searchTextBox.BorderStyle = BorderStyle.FixedSingle;
-            searchTextBox.Size = new Size(568, 27);
+            searchTextBox.Size = new Size(648, 27);
             searchTextBox.TabIndex = 0;
             searchTextBox.TextChanged += SearchTextBox_TextChanged;
             //
@@ -56,16 +56,18 @@ namespace WindowDeck
             topBar.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             topBar.ColumnCount = 3;
             topBar.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-            topBar.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 36F));
-            topBar.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 36F));
-            topBar.Controls.Add(searchTextBox, 0, 0);
+            topBar.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 44F));
+            topBar.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 44F));
             topBar.Controls.Add(helpButton, 1, 0);
             topBar.Controls.Add(settingsButton, 2, 0);
-            topBar.Location = new Point(16, 12);
+            topBar.Controls.Add(searchTextBox, 0, 1);
+            topBar.SetColumnSpan(searchTextBox, 3);
+            topBar.Location = new Point(16, 8);
             topBar.Name = "topBar";
-            topBar.RowCount = 1;
+            topBar.RowCount = 2;
+            topBar.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
             topBar.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            topBar.Size = new Size(648, 36);
+            topBar.Size = new Size(648, 76);
             topBar.TabIndex = 0;
             //
             // windowListPanel
@@ -74,9 +76,9 @@ namespace WindowDeck
             windowListPanel.AutoScroll = true;
             windowListPanel.BorderStyle = BorderStyle.None;
             windowListPanel.FlowDirection = FlowDirection.TopDown;
-            windowListPanel.Location = new Point(16, 56);
+            windowListPanel.Location = new Point(16, 92);
             windowListPanel.Name = "windowListPanel";
-            windowListPanel.Size = new Size(648, 636);
+            windowListPanel.Size = new Size(648, 596);
             windowListPanel.TabIndex = 1;
             windowListPanel.WrapContents = false;
             //
@@ -84,27 +86,28 @@ namespace WindowDeck
             //
             bottomBar.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             bottomBar.Controls.Add(presentationModeButton);
-            bottomBar.Location = new Point(16, 700);
+            bottomBar.Location = new Point(16, 696);
             bottomBar.Name = "bottomBar";
-            bottomBar.Size = new Size(648, 38);
+            bottomBar.Size = new Size(648, 42);
             bottomBar.TabIndex = 2;
             //
             // bottom bar buttons
             //
             presentationModeButton.Anchor = AnchorStyles.Top | AnchorStyles.Left;
             presentationModeButton.FlatStyle = FlatStyle.Flat;
+            presentationModeButton.Font = new Font("Segoe UI", 9.5F, FontStyle.Bold);
             presentationModeButton.Location = new Point(0, 4);
-            presentationModeButton.Size = new Size(180, 30);
+            presentationModeButton.Size = new Size(190, 34);
             helpButton.Anchor = AnchorStyles.None;
             helpButton.FlatStyle = FlatStyle.Flat;
-            helpButton.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            helpButton.Margin = new Padding(2);
-            helpButton.Size = new Size(32, 32);
+            helpButton.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            helpButton.Margin = new Padding(3);
+            helpButton.Size = new Size(38, 34);
             settingsButton.Anchor = AnchorStyles.None;
             settingsButton.FlatStyle = FlatStyle.Flat;
-            settingsButton.Font = new Font("Segoe UI Symbol", 11F, FontStyle.Regular);
-            settingsButton.Margin = new Padding(2);
-            settingsButton.Size = new Size(32, 32);
+            settingsButton.Font = new Font("Segoe UI Symbol", 12F, FontStyle.Regular);
+            settingsButton.Margin = new Padding(3);
+            settingsButton.Size = new Size(38, 34);
             //
             // Form1
             //
